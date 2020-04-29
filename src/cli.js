ff5b9d03f250b607d3f987b819928d56eff46bc9
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-04-29 15:24:27 +0200

    Performs Task 3

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
+import readlineSync from 'readline-sync';
 export const startDialog = () => {
-  let readlineSync = require('readline-sync');
-
-  let userName = readlineSync.question('May I have your name, dear? ');
+  let userName = readlineSync.question('May I have your name,  my dear? ');
   console.log('Hi' + userName + ':))');
 
 }
